f9887024def0d4ce354630c4b720bf66a79877db
dulaj1234 <dulajsam@gmail.com>
2026-03-16 19:00:00 +0530

    create stock price table

--- app/database.py
+++ app/database.py
@@ -11,3 +11,24 @@ def get_connection():
     conn = sqlite3.connect(DB_PATH, check_same_thread=False)
     conn.row_factory = sqlite3.Row
     return conn
+
+def init_db():
+    """
+    Creates the table if it doesn't already exist.
+    """
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS monthly_stock_prices (
+            symbol  TEXT NOT NULL,
+            date    TEXT NOT NULL,
+            high    TEXT NOT NULL,
+            low     TEXT NOT NULL,
+            volume  INTEGER NOT NULL,
+            PRIMARY KEY (symbol, date)
+        )
+    """)
+
+    conn.commit()
+    conn.close()
